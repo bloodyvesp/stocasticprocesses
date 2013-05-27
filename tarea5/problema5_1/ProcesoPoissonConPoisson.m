6f4bb962cda95e98d7bb8c18d29df2f938f44c8b
htellez <htellez@nearbpo.com>
2013-05-27 02:24:49 -0500

    Corrección y notas en 5.1.4

--- tarea5/problema5_1/ProcesoPoissonConPoisson.m
+++ tarea5/problema5_1/ProcesoPoissonConPoisson.m
@@ -5,8 +5,12 @@ clear;
 
 #Se calcula la cantidad de saltos al tiempo t
 	N_t = poissrnd(lambda*50);
-	T = sort(rand(N_t, 1)) * t;
-	T(N_t) = T(N_t - 1);
-	
+	T(N_t) = t;
+	T(1)=0;
+	for i = 1:(N_t-1)
+		T(i+1) = t1_dado_tn_rnd(N_t - i + 1, t - T(i));
+		T(i+1) = T(i+1) + T(i);
+	endfor;
+	T = T(2:end);
 
 GraficaPoisson(T, lambda);
