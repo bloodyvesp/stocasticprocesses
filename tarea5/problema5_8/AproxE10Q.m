753dfad4c70dee1b0f770d64a234a014fdc3344f
htellez <htellez@nearbpo.com>
2013-06-03 07:11:39 -0500

    Se termina 5.8

--- tarea5/problema5_8/AproxE10Q.m
+++ tarea5/problema5_8/AproxE10Q.m
@@ -1,15 +1,10 @@
-function r = AproxE10Q(h)
-	Q = [-2 2; 3 -3];
+function P = AproxE10Q(h)
+    Q = [-2 2; 3 -3];
 
-
-	lim = floor(10/h);
-
-	P = eye(2);
-	for i=[1:lim]
-		R = h*(P*Q);
-		P = P + R;
-	endfor
-	
-	P
-	r=max(max(R));
+    P = eye(2);
+    lim = floor(10/h);
+    for i=[1:lim]
+        R = h*(Q*P);
+        P = P + R;
+    endfor
 endfunction
--- tarea5/problema5_8/IteraAproxE10Q.m
+++ tarea5/problema5_8/IteraAproxE10Q.m
@@ -1,8 +1,10 @@
-h = 1;
+h = 1/3;
 r=1;
+i=1;
+E10Q = expm(10*[-2 2; 3 -3]);
 while( r > 0.000001)
-	r = AproxE10Q(h);
-	h = h - h/10000;
+    r = max(max(abs(AproxE10Q(h) - E10Q)));
+    h = h - h/10000;
+    i=i+1;
 endwhile
-"terminó"
 h
